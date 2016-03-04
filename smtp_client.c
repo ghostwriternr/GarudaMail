44be87be3c6382b002cfff0f91fc7751a4dc4b63
Vikas Choudhary <cvikas54@gmail.com>
2016-03-04 14:21:18 +0530

    implement SAML, read this https://www.cs.cf.ac.uk/Dave/PERL/node175.html

--- smtp_client.c
+++ smtp_client.c
@@ -6,14 +6,18 @@
 #include <string.h> // For memset.
 #include <arpa/inet.h> // For inet_pton (), inet_ntop ().
 
-int port_num = 21436;
+int port_num = 21439;
 
 #define BUF_SIZE 10000
 
+void interact(int sfd, char S[]);
+void DATA(int sfd, char S[]);
+
 int main (int argc, char *argv[])
 {
-while (1)
-  {
+// while (1)
+//   {
+	char buf[10000] = {"\0"};
 	int rst; // Return status of functions.
 	int cfd; // File descriptor for the client.
 
@@ -45,22 +49,98 @@ while (1)
 	  perror ("Client Presentation to network address conversion.\n");
 	  exit (1);
 	}
-
 	/***************** Connect to the server ************************/
 	rst = connect (sfd, (struct sockaddr *) &srv_addr, addrlen);
-	if (rst == -1)
+	rst = recv(sfd, buf, BUF_SIZE, 0);
+    printf("%s\n",buf);
+    memset(buf,'\0',strlen(buf));
+    char S[10000],ch;
+    int i,flag=0,check=1;
+    while(1)
+    {
+    	i=0;
+    	while(1)
+    		{
+    			ch=getchar();
+    			if(ch=='\n')
+    				break;
+    			S[i++]=ch;
+    		}
+    	S[i]='\0';
+    	printf("scanned %s\n",S);
+    	if(strstr(S,"HELO ") != NULL && flag==0)
+    	{
+    		interact(sfd, S);
+    		flag=1;
+    	}
+    	else if(strstr(S,"MAIL FROM ") != NULL && flag==1)
+    	{
+    		interact(sfd, S);
+    		flag==2;
+    		check=0;
+    	}
+    	else if(strstr(S,"RCPT TO ") != NULL && check==0)	// can be multiple receivers
+    	{
+    		interact(sfd, S);
+    		flag=3;
+    	}
+    	else if(strcmp(S,"DATA")==0 && flag==3)	//content will be entered from next line onwards
+    	{
+    		DATA(sfd, S);
+    		flag=4;
+    	}
+    	else if(strcmp(S,"QUIT")==0)	//content will be entered from next line onwards
+    	{
+    		interact(sfd, S);
+    		break;
+    	}
+    	else
+    		printf("> command not found!\n");
+    	printf("> ");
+    }
+    if (rst == -1)
 	{
 	  perror ("Client: Connect failed.");
 	  exit (1);
 	}
 
-	//continue from here onwards
-	
-	char buf[BUF_SIZE] = {'\0'}, *token, a[3];
-	printf("Your Name\n");
-	scanf("%s", buf);
-	rst = send(sfd, buf, strlen(buf), 0);
 	rst = close (sfd);
+	return 0;
 }
-  return 0;
+
+void interact(int sfd,char S[])
+{
+    int rst;
+	char buf[BUF_SIZE] = {'\0'};
+    strcpy(buf,S);
+	rst = send(sfd, buf, strlen(buf), 0);
+    memset(buf,'\0',strlen(buf));
+    rst = recv(sfd, buf, BUF_SIZE, 0);
+    printf("%s\n",buf);
+}
+
+void DATA(int sfd, char S[])
+{
+	int i, rst;
+	char ch;
+	interact(sfd,S);
+	printf("> ");
+	while(1)
+	{
+		memset(S,'\0',strlen(S));
+    	i=0;
+    	while(1)
+    		{
+    			ch=getchar();
+    			if(ch=='\n')
+    				break;
+    			S[i++]=ch;
+    		}
+    	S[i]='\0';
+		printf("> ");
+		rst = send(sfd, S, strlen(S), 0);
+		for(i=0;i<strlen(S);i++)
+		if(S[i]=='.')
+			break;
+	}
 }
--- smtp_server.c
+++ smtp_server.c
@@ -13,7 +13,15 @@
 
 #define BUF_SIZE  10000
 char  path[100] = {"./articles/"}, data[1000] = {"./data/"};
-int   port_num  = 23464;
+int   port_num  = 21439;
+
+void serviceready( int cfd );
+void receive(int cfd, char buf[]);
+void HELO(int cfd,char buf[],char domain[]);
+void MAILFROM(int cfd,char buf[],char sender[]);    
+void RCPTTO(int cfd,char buf[],char receivers[][500],int *rsize);
+void DATA(int cfd,char data[]);
+void done(int cfd,char domain[],char sender[],char receivers[][500],int rsize,char data[]);
 
 int main( )
 {
@@ -48,7 +56,7 @@ int main( )
     // Assign values to the server address.
     srv_addr.sin_family = AF_INET; // IPv4.
     //printf("port: %d\n",22436+atoi(argv[1]));
-    srv_addr.sin_port   = htons (21436); // Port Number.
+    srv_addr.sin_port   = htons (port_num); // Port Number.
 
     rst = inet_pton (AF_INET, "127.0.0.1", &srv_addr.sin_addr); /* To
                               * type conversion of the pointer here. */
@@ -100,16 +108,44 @@ int main( )
             perror ("In Server: Accept failed");
             exit (1);
         }
-      
     if (fork() == 0)
       {
         close(sfd);
         char buf[BUF_SIZE] = {'\0'};
-        rst = recv(cfd, buf, BUF_SIZE, 0);
-        if(rst==-1)
-            printf("receive failed!\n");
-        else
-            printf("Received : %s\n",buf);
+        char domain[100] = {'\0'};
+        char sender[500] = {'\0'};
+        char receivers[20][500] = {'\0'};
+        char data[10000] = {'\0'};
+        int rsize=0;
+        serviceready(cfd);
+        while(1)
+        {
+            receive(cfd,buf);
+            if(strstr(buf,"HELO ") != NULL)
+            {
+                HELO(cfd, buf, domain);
+            }
+            else if(strstr(buf,"MAIL FROM ") != NULL)
+            {
+                MAILFROM(cfd, buf, sender);    
+            }
+            else if(strstr(buf,"RCPT TO ") != NULL)   // can be multiple receivers
+            {
+                RCPTTO(cfd, buf, receivers, &rsize);
+            }
+            else if(strcmp(buf,"DATA")==0) //content will be entered from next line onwards
+            {
+                DATA(cfd,data);
+                done(cfd, domain, sender, receivers, rsize, data);
+            }
+            else if(strcmp(buf,"QUIT")==0 ) //content will be entered from next line onwards
+            {
+                memset(buf,'\0',strlen(buf));
+                strcpy(buf,"221 service closed");
+                rst = send(cfd, buf, strlen(buf), 0);
+                break;
+            }
+        }
         rst = close(cfd);
         if (rst == -1)
         {
@@ -121,3 +157,91 @@ int main( )
     }
   return 0;
 }
+
+void serviceready( int cfd )
+{
+    int rst;
+    char buf[BUF_SIZE] = {'\0'};
+    strcpy(buf,"220 service ready");
+    rst = send(cfd, buf, strlen(buf), 0);
+}
+
+void receive(int cfd, char buf[])
+{
+    int rst;
+    memset(buf,'\0',strlen(buf));
+    rst = recv(cfd, buf, BUF_SIZE, 0);
+    printf("Received : %s\n",buf);
+}
+
+void HELO(int cfd,char buf[],char domain[])
+{
+    int rst;
+    int i;
+    for(i=0;buf[i]!=' ';i++);
+    strcpy(domain,&buf[i+1]);
+    memset(buf,'\0',strlen(buf));
+    strcpy(buf,"250 OK");
+    rst = send(cfd, buf, strlen(buf), 0);
+}
+
+void MAILFROM(int cfd,char buf[],char sender[])
+{
+    int rst;
+    int i;
+    for(i=0;buf[i]!=' ';i++);
+    strcpy(sender,&buf[i+1]);
+    memset(buf,'\0',strlen(buf));
+    strcpy(buf,"250 OK");
+    rst = send(cfd, buf, strlen(buf), 0);
+}    
+
+void RCPTTO(int cfd,char buf[],char receivers[][500],int *rsize)
+{
+    int rst;
+    int i;
+    for(i=0;buf[i]!=' ';i++);
+    strcpy(receivers[*rsize],&buf[i+1]);
+    *rsize = *rsize + 1;
+    memset(buf,'\0',strlen(buf));
+    strcpy(buf,"250 OK");
+    rst = send(cfd, buf, strlen(buf), 0);
+}
+
+void DATA(int cfd,char data[])
+{
+    char buf[1000];
+    int rst;
+    strcpy(buf,"354 start mail input");
+    rst = send(cfd, buf, strlen(buf), 0);
+    do{
+        memset(buf,'\0',strlen(buf));
+        receive(cfd,buf);
+        strcat(data,buf);
+    }while(strstr(buf,".")==NULL);
+}
+
+void done(int cfd,char domain[],char sender[],char receivers[][500],int rsize,char data[])
+{
+    printf("domain : %s\n",domain);
+    printf("sender : %s\n",sender);
+    int i;
+    for(i=0; i < rsize;i++)
+    printf("receivers[%d] : %s\n",i+1,receivers[i]);
+    printf("data : %s\n",data);
+}
+
+/**
+    int rst;
+    char buf[BUF_SIZE] = {'\0'};
+    strcpy(buf,"220 service ready");
+    rst = send(cfd, buf, strlen(buf), 0);
+    memset(buf,'\0',strlen(buf));
+    rst = recv(cfd, buf, BUF_SIZE, 0);
+    if(rst==-1)
+        printf("Sending failed!\n");
+    printf("%s\n",buf);
+    memset(buf,'\0',strlen(buf));
+    strcpy(buf,"250 OK");
+    rst = send(cfd, buf, strlen(buf), 0);
+*/
